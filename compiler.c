01fb7dd6403fc37d0768459d383b94d134ddf28c
aobrien68 <68aobrien@gmail.com>
2025-04-29 16:55:28 -0400

    Initial Commit

---
+++ compiler.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+enum Token {
+	INVALID,
+	MOV,
+	ADD,
+	XOR,
+	SUB,
+
+}
+
+int main(int argc, char *argv[]) {
+	FILE *source = fopen(argv[1]);
+	
+}
+
+Token getNextToken(FILE *stream) {
+	
+}
